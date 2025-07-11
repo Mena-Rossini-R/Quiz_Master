10baf0f1dc4d10df915f06fa47e06cbda23ecaa3
Mena-Rossini-R <mena.rossini.r@gmail.com>
2025-07-11 11:43:15 +0530

    done

---
+++ script.js
@@ -0,0 +1,32 @@
+function calculateScore(callback) {
+    const userAnswers = [
+        document.getElementById('q1').value.trim(),
+        document.getElementById('q2').value.trim(),
+        document.getElementById('q3').value.trim()
+    ];
+
+    const correctAnswers = ["Paris", "1332", "Snow"];
+    const score = callback(userAnswers, correctAnswers);
+
+    document.getElementById("result").textContent = `Your Score: ${score} / ${correctAnswers.length}`;
+}
+
+function strictScoring(userAns, correctAns) {
+    let score = 0;
+    for (let i = 0; i < correctAns.length; i++) {
+        if (userAns[i] === correctAns[i]) {
+            score++;
+        }
+    }
+    return score;
+}
+
+function lenientScoring(userAns, correctAns) {
+    let score = 0;
+    for (let i = 0; i < correctAns.length; i++) {
+        if (userAns[i].toLowerCase() === correctAns[i].toLowerCase()) {
+            score++;
+        }
+    }
+    return score;
+}
